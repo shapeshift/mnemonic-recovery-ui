a6655ada400d942994f0016837938b60cbf4a619
reallybeard <89934888+reallybeard@users.noreply.github.com>
2023-10-18 11:12:07 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ import { FaGithub } from 'react-icons/fa'
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Flex flexDir='column' p={8} className="App" bgImage={Bg} backgroundSize='cover' minHeight='100vh'>
-        <Container mb={6} my='auto'>
-          <Card backdropFilter='blur(50px)' overflow='hidden' borderWidth={1}>
+      <Flex flexDir='column' p={{ base: 0, md: 8}} className="App" bgImage={Bg} backgroundSize='cover' minHeight='100vh'>
+        <Container mb={6} my='auto' px={0}>
+          <Card borderRadius={{ base: 0, sm: 'xl'}} backdropFilter='blur(50px)' overflow='hidden' borderWidth={1}>
             <CardHeader px={8} pt={8} pb={0}>
               <Heading fontSize='2xl'>Decrypt Secret Recovery Phrase</Heading>
               <Text mt={2} color='text.subtle' fontSize='sm' fontWeight='medium'>If you received an email from ShapeShift with an encrypted wallet string, paste the string along with the email and password you set when your wallet was created to view your Secret Recovery Phrase.</Text>
@@ -19,7 +19,7 @@ function App() {
           <SeedForm />
           </Card>
         </Container>
-        <Link fontSize='xl' mt='auto' mx='auto' isExternal href='https://github.com/shapeshift/mnemonic-recovery-ui'>
+        <Link fontSize='xl' mt={{ base: 4, md: 'auto'}} mx='auto' isExternal href='https://github.com/shapeshift/mnemonic-recovery-ui'>
           <FaGithub />
         </Link>
       </Flex>
